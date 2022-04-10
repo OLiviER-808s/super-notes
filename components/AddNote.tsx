631c45e7a82096bb4d282789625c153819fc6e67
Olivier Bruch <olivier.bruch@gmail.com>
2022-04-10 12:43:20 +0100

    add text note functionality added

--- components/AddNote.tsx
+++ components/AddNote.tsx
@@ -1,26 +1,63 @@
 import { Button, Group, Modal, Textarea, TextInput } from "@mantine/core"
+import { useForm } from "@mantine/form"
 import { IconMusic, IconNote, IconPhoto } from "@tabler/icons"
 import { useState } from "react"
+import { auth, db, timestamp } from "../lib/firebase"
+import { useAuthState } from "react-firebase-hooks/auth";
+import Note from "../models/Note.model"
+import { addDoc, collection } from "firebase/firestore"
 
 const AddNote = () => {
   const [opened, setOpened] = useState(false)
+  const [user] = useAuthState(auth)
+
+  const form = useForm({
+    initialValues: {
+      title: '',
+      content: ''
+    }
+  })
+
+  const handleSubmit = async () => {
+    if (form.values.title || form.values.content) {
+      const note: Note = {
+        title: form.values.title,
+        content: form.values.content,
+        pinned: false,
+        createdAt: timestamp(),
+        uid: user?.uid
+      }
+
+      const ref = collection(db, 'notes')
+      addDoc(ref, note)
+
+      closeModal()
+    }
+  }
+
+  const closeModal = () => {
+    setOpened(false)
+    form.reset()
+  }
 
   return (
     <>
-      <Modal opened={opened} onClose={() => setOpened(false)} title="Create A New Note">
-        <form className="form">
-          <TextInput placeholder="Note title" label="Title"/>
+      <Modal opened={opened} onClose={closeModal} title="Create A New Note">
+        <form className="form" onSubmit={form.onSubmit(handleSubmit)}>
+          <TextInput placeholder="Note title" label="Title" 
+          {...form.getInputProps('title')}/>
 
           <Group position="center" spacing="xl">
             <Button variant="outline" radius="xl" size="xs" leftIcon={<IconPhoto />}>Add Image</Button>
             <Button variant="outline" radius="xl" size="xs" color="orange" leftIcon={<IconMusic />}>Add Audio</Button>
           </Group>
 
-          <Textarea autosize minRows={5} placeholder="Your Content" label="Content"/>
+          <Textarea autosize minRows={5} placeholder="Your Content" label="Content"
+          {...form.getInputProps('content')}/>
 
           <Group position="right" style={{'margin': '0'}}>
-            <Button variant="light">Cancel</Button>
-            <Button color="green">Add Note</Button>
+            <Button variant="light" onClick={closeModal}>Cancel</Button>
+            <Button color="green" type="submit">Add Note</Button>
           </Group>
         </form>
       </Modal>
---
+++ models/Note.model.ts
@@ -0,0 +1,11 @@
+export default interface Note {
+  title?: string;
+  content?: string;
+  photo?: string;
+  audio?: string;
+  path?: string;
+  pinned?: boolean;
+  createdAt?: any;
+  uid?: string;
+  id?: string;
+}
